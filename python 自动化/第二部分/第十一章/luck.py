d874f7bd5daf6e49653871d21696831e63938237
Sherlock-Ni <chenhaoni8@hotmail.com>
2020-05-28 17:03:18 +0800

    从web获取信息

---
+++ python 自动化/第二部分/第十一章/luck.py
@@ -0,0 +1,18 @@
+#! python
+# lucky.py - Open several Google search result
+
+import requests, sys, webbrowser, bs4
+
+print('Googling...')
+res = requests.get('http://bing.com/search?q=:' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+#Retrieve top search result links
+soup = bs4.BeautifulSoup(res.text)
+
+#Open a browser tab for each result 
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://bing.com' + linkElems[i].get('href'))
+
